be9c9977a0576d79820dedb42f88a3ca1e32d303
E-Viskhan <viskhan11@gmail.com>
2022-05-12 22:44:47 +0300

    Added Touchable Opacity when click on cards

--- components/Home/MainCards/BalanceCard.jsx
+++ components/Home/MainCards/BalanceCard.jsx
@@ -1,11 +1,16 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { Box, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 export default function BalanceCard() {
     return (
-        <Box w='48%' bg='#edf8f7' p={10} borderRadius={13} justifyContent='center' alignItems='center'>
-            <FontAwesome name="balance-scale" size={60} color="#616161"/>
-            <Text mt={4} textTransform='uppercase' bold color='gray.700'>Баланс</Text>
+        <Box w='48%'>
+            <TouchableOpacity style={{ flex: 1 }}>
+                <Box h='100%' bg='#edf8f7' p={10} borderRadius={13} justifyContent='center' alignItems='center'>
+                    <FontAwesome name="balance-scale" size={60} color="#616161"/>
+                    <Text mt={4} textTransform='uppercase' bold color='gray.700'>Баланс</Text>
+                </Box>
+            </TouchableOpacity>
         </Box>
     );
 };
--- components/Home/MainCards/PlusCard.jsx
+++ components/Home/MainCards/PlusCard.jsx
@@ -1,11 +1,14 @@
 import { Box, Text } from "native-base";
 import { FontAwesome } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 export default function PlusCard() {
     return (
-        <Box bg='#f8ede8' mb={3} p={10} borderRadius={13} justifyContent='center' alignItems='center'>
-            <FontAwesome name="plus" size={30} color="#616161" />
-            <Text mt={4} textTransform='uppercase' bold color='gray.700'>Покупка</Text>
-        </Box>
+        <TouchableOpacity>
+            <Box bg='#f8ede8' mb={3} p={10} borderRadius={13} justifyContent='center' alignItems='center'>
+                <FontAwesome name="plus" size={30} color="#616161" />
+                <Text mt={4} textTransform='uppercase' bold color='gray.700'>Покупка</Text>
+            </Box>
+        </TouchableOpacity>
     );
 };
